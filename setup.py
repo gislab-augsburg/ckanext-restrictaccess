1f5806cc4f211e9071a2e2917f95557ce8e571d2
Martin <mb@geoinfo-bayr.de>
2024-10-30 17:40:40 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     author='Your Name',
     author_email='your.email@example.com',
     license='AGPL',
-    url='https://github.com/your-repo/ckanext-restrictaccess',
-    packages=find_packages(),
+    url='https://github.com/gislab-augsburg/ckanext-restrictaccess',
+    packages=find_packages(include=['ckanext', 'ckanext.*']),
     include_package_data=True,
     zip_safe=False,
     entry_points='''
